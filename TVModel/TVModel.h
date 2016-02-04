27e0ab15a9e63deea2daf980a34a3fd1fcf19f5d
Xabier I. Losada <hi@xilosada.com>
2016-02-04 13:54:15 +0100

    First commit. App skeleton with Carthage dependencies

---
+++ TVModel/TVModel.h
@@ -0,0 +1,19 @@
+//
+//  TVModel.h
+//  TVModel
+//
+//  Created by X.I. Losada on 04/02/16.
+//  Copyright © 2016 XiLosada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TVModel.
+FOUNDATION_EXPORT double TVModelVersionNumber;
+
+//! Project version string for TVModel.
+FOUNDATION_EXPORT const unsigned char TVModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TVModel/PublicHeader.h>
+
+
